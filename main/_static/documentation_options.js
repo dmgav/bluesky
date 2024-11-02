a0d3c3e9fa6310c89f0c379a84a0a4e9e1dd283c
dmgav <dmgav@users.noreply.github.com>
2024-11-02 14:17:38 +0000

    deploy: 8ad45bf5ee94a10d12eabdf8ab694ba3ac09ca2d

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.6.0rc2.dev1478+g26cde4a6',
+    VERSION: '1.6.0rc2.dev1489+g8ad45bf5',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
