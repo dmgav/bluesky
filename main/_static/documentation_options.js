0b9018515972111b47e70539476765be0c760bb4
dmgav <dmgav@users.noreply.github.com>
2026-06-18 13:23:52 +0000

    deploy: d276e2bf87ed6bc5cd8e1d20de605f7fec9812ca

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.6.0rc2.dev1871+g7c59ad71b',
+    VERSION: '1.6.0rc2.dev2046+gd276e2bf8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
